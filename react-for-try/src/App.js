a5c599d5c9a56df25ff4fce0264bd581357f0415
fjhgf <fjhgf@naver.com>
2024-12-24 10:26:26 +0900

    6.0

--- react-for-try/src/App.js
+++ react-for-try/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
+
+//import Button from "./Button";
+//import styles from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1); // prev : 이전 값
+  console.log("claa api"); // 함수가 rendering되는걸 확인 > APP함수가 모두 render됌
   return (
     <div className="App">
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
