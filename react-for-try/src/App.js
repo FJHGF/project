0f67182051ea07b1188856a4b92a7c3e3fdfa16a
fjhgf <fjhgf@naver.com>
2024-12-25 20:23:47 +0900

    6.2

--- react-for-try/src/App.js
+++ react-for-try/src/App.js
@@ -9,10 +9,28 @@ function App() {
   const [keyword, serKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1); // prev : 이전 값
   const onChange = (event) => serKeyword(event.target.value);
-  console.log("i run all the time "); // 계속 rendering
+  //console.log("i run all the time "); // 계속 rendering
   useEffect(() => {
-    console.log("CALL THE API...");
+    //console.log("CALL THE API...");
+    console.log("I run only once.");
   }, []);
+  useEffect(() => {
+    //console.log(1);
+    /*
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR ", keyword);
+    }
+    */
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' or 'counter' changes.");
+  }, [keyword, counter]);
   return (
     <div className="App">
       <input
