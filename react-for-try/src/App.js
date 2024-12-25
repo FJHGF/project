4821ff8618a1a8fe283397642208226a2898f529
fjhgf <fjhgf@naver.com>
2024-12-25 19:56:23 +0900

    6.2

--- react-for-try/src/App.js
+++ react-for-try/src/App.js
@@ -6,13 +6,21 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, serKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1); // prev : 이전 값
+  const onChange = (event) => serKeyword(event.target.value);
   console.log("i run all the time "); // 계속 rendering
   useEffect(() => {
     console.log("CALL THE API...");
   }, []);
   return (
     <div className="App">
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search hear"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
