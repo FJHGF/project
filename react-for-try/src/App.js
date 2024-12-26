adcab1f1756826e05c30067af55665b368a77056
fjhgf <fjhgf@naver.com>
2024-12-26 15:15:14 +0900

    7.0

--- react-for-try/src/App.js
+++ react-for-try/src/App.js
@@ -1,39 +1,38 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function byFn() {
-    console.log("bye");
-  }
-  function hiFn() {
-    console.log("I'm here!");
-    return byFn;
-  }
-  // 1 > 코드가 더 간결함
-  useEffect(() => {
-    console.log("hi");
-    return () => {
-      console.log("bye");
-    };
-  }, []);
-  //2
-  useEffect(function () {
-    console.log("hi!!");
-    return function () {
-      console.log("bye!!");
-    };
-  }, []);
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault(); // form태그 submit할 때 브라우저가 새로고침을 방지
+    //console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+
+    setToDos((currentArray) => [toDo, ...currentArray]); //직접 값을 넣거나 함수를 넣기
+    // 새로운 값 + 현재 배열값(...currentArray)(배열 내 값만 남음)
+    setToDo(""); // input 태그 내용 지우기
+  };
+  //console.log(toDo);
+  console.log(toDos); //state를 수정하고 싶으면 modify를 통해서 수정
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
