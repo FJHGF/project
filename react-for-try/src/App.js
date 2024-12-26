2f9f53531b2b2dd814235292ce84122fd37a3869
fjhgf <fjhgf@naver.com>
2024-12-26 15:30:26 +0900

    7.1

--- react-for-try/src/App.js
+++ react-for-try/src/App.js
@@ -9,18 +9,17 @@ function App() {
     setToDo(event.target.value);
   };
   const onSubmit = (event) => {
-    event.preventDefault(); // form태그 submit할 때 브라우저가 새로고침을 방지
-    //console.log(toDo);
+    event.preventDefault();
+
     if (toDo === "") {
       return;
     }
 
-    setToDos((currentArray) => [toDo, ...currentArray]); //직접 값을 넣거나 함수를 넣기
-    // 새로운 값 + 현재 배열값(...currentArray)(배열 내 값만 남음)
-    setToDo(""); // input 태그 내용 지우기
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
   };
   //console.log(toDo);
-  console.log(toDos); //state를 수정하고 싶으면 modify를 통해서 수정
+  console.log(toDos);
   return (
     <div>
       <h1>My To Dos({toDos.length})</h1>
@@ -33,6 +32,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
